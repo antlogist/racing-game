3fe7bc468eecc3e1bb0eaa5a0084c214a519aff3
antlogist <antlogist@gmail.com>
2022-02-21 23:03:46 +0300

    Car start position added

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,9 @@ const trackGap = 2;
 const trackCols = 20;
 const trackRows = 15;
 
+var trackX = 0;
+var trackY = 0;
+
 const trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                    1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
                    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -15,7 +18,7 @@ const trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                    1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
                    1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
                    1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-                   1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                   1, 2, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
                    1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
                    1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
                    1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
@@ -31,6 +34,8 @@ window.onload = function() {
 	const framesPerSecond = 30;
 	setInterval(updateAll, 1000/framesPerSecond);
 
+  carReset();
+
 }
 
 function updateAll() {
@@ -41,6 +46,21 @@ function rowColToArrayIndex(col, row) {
 	return col + trackCols * row;
 }
 
+function carReset() {
+	for(let eachRow = 0; eachRow < trackRows; eachRow++) {
+		for(let eachCol=0; eachCol < trackCols; eachCol++) {
+
+			const arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+
+			if(trackGrid[arrayIndex] == 2) {
+				trackGrid[arrayIndex] = 0; //become a plain road
+				trackX = eachCol * trackWidth + trackWidth/2;
+				trackY = eachRow * trackHeight + trackHeight/2;
+			} // end of is this track here
+		} // end of for each track
+	} // end of for each row
+}
+
 
 function drawTracks() {
 
@@ -49,7 +69,7 @@ function drawTracks() {
 
 			const arrayIndex = rowColToArrayIndex(eachCol, eachRow);
 
-			if(trackGrid[arrayIndex]) {
+			if(trackGrid[arrayIndex] === 1) {
 				colorRect(trackWidth * eachCol,trackHeight * eachRow,
 					trackWidth - trackGap, trackHeight - trackGap, 'coral');
 			} // end of is this track here
@@ -61,11 +81,18 @@ function drawTracks() {
 
 function drawAll() {
 	colorRect(0,0, canvas.width,canvas.height, 'black'); // clear screen
-
+	colorCircle(trackX,trackY, 10, 'white'); // draw track as a circle
 	drawTracks();
 }
 
 function colorRect(topLeftX,topLeftY, boxWidth,boxHeight, fillColor) {
 	canvasContext.fillStyle = fillColor;
 	canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+}
+
+function colorCircle(centerX,centerY, radius, fillColor) {
+	canvasContext.fillStyle = fillColor;
+	canvasContext.beginPath();
+	canvasContext.arc(centerX,centerY, 10, 0,Math.PI*2, true);
+	canvasContext.fill();
 }
